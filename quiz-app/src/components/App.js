38120b3950d098e6bb045d4318d30ccb0bb1f8ed
Kim Connolly <77863598+kimconn@users.noreply.github.com>
2023-10-18 18:03:50 -0400

    fix: navbar import

--- quiz-app/src/components/App.js
+++ quiz-app/src/components/App.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme';
+import Nav from './Nav';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <App />
+      <Nav />
     </ChakraProvider>
   );
 }
