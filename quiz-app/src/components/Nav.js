e636c8465e7e35a06802a2ccc8a862639964e16b
Kim Connolly <77863598+kimconn@users.noreply.github.com>
2023-10-21 14:39:12 -0400

    update logo in nav

--- quiz-app/src/components/Nav.js
+++ quiz-app/src/components/Nav.js
@@ -15,7 +15,11 @@ export default function Nav() {
       borderBottom={'2px solid #969BAB'}
     >
       <Box>
-        <Image src="quiz-app/public/assets/booklogo2.jpg" />
+        <Image
+          boxSize="70%"
+          objectFit="contain"
+          src="https://i.ibb.co/FsLT19z/booklogo3.png"
+        />
       </Box>
       <Flex gap={4} align="center">
         <Button href="#" bg={'#D2B6FF'} onClick={() => setModalOpen(true)}>
