62964380002fa8553c6e2a3b6af503db93408203
Kim Connolly <77863598+kimconn@users.noreply.github.com>
2023-10-21 13:25:35 -0400

    implement showQuiz and quizMode states

--- quiz-app/src/components/BookReader.tsx
+++ quiz-app/src/components/BookReader.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '@chakra-ui/react';
 import { ReactReader } from 'react-reader';
 
 export default function BookReader({ selectedBook }) {
+  const [location, setLocation] = useState<string | number>(0);
   return (
-    <Box style={{ height: '100vh' }}>
+    <Box style={{ height: '70vh' }}>
       <ReactReader
         url={selectedBook.url}
-        location={0}
-        locationChanged={(epubcifi) => {}}
+        location={location}
+        locationChanged={(epubcifi) => setLocation(epubcifi)}
       />
     </Box>
   );
