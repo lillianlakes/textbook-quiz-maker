62a5f3c30a04d594b5bb77e588d51a6d964b97ef
Kim Connolly <77863598+kimconn@users.noreply.github.com>
2023-10-21 13:16:12 -0400

    feat: Footer for quiz mode

---
+++ quiz-app/src/components/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Menu,
+  MenuList,
+  MenuItem,
+  MenuButton,
+  Flex,
+  Button,
+  ButtonGroup,
+} from '@chakra-ui/react';
+
+export default function Footer() {
+  return (
+    <Flex direction="row" my={3} justify="space-between">
+      <Menu>
+        <MenuButton
+          size="md"
+          p={2}
+          border="1px solid black"
+          _active={{
+            bg: '#FFD465',
+            transform: 'scale(0.98)',
+          }}
+        >
+          Aa
+        </MenuButton>
+        <MenuList minW="none" bg="#FFD465">
+          <MenuItem fontFamily="sans-serif" bg="#FFD465">
+            Sans Serif
+          </MenuItem>
+          <MenuItem fontFamily="revert" bg="#FFD465">
+            Bionic Read
+          </MenuItem>
+        </MenuList>
+        <ButtonGroup>
+          <Button size="md">Highlights</Button>
+          <Button size="md">Quiz</Button>
+        </ButtonGroup>
+      </Menu>
+    </Flex>
+  );
+}
